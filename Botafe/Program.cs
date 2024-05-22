6cd5c96c35f7f1fe468bc55b4db9e9933902460a
Lucas Cichon <lucas.cichon93@gmail.com>
2024-05-22 09:33:43 +0200

    configure Serilog

--- Botafe/Program.cs
+++ Botafe/Program.cs
@@ -2,6 +2,7 @@
 using Botafe.Application;
 using Botafe.Infrastructure;
 using Botafe.Persistance;
+using Serilog;
 
 namespace Botafe
 {
@@ -9,45 +10,63 @@ namespace Botafe
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddApplication();
-            builder.Services.AddPersistance(builder.Configuration);
-            builder.Services.AddInfrastructure(builder.Configuration);
-            builder.Services.AddCors(options => options.AddPolicy(name: "MyAllowSpecificOrigins",
-                builder =>
-                {
-                    builder.WithOrigins("https://localhost:44376", "https://localhost:7267");
-                }));
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(c => SwaggerDocExtensions.SetSwaggerDoc(c));
-            builder.Services.AddHealthChecks();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                //var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+         
+                var builder = WebApplication.CreateBuilder(args);
+                //Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger();
+
+                builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
+                builder.Services.AddApplication();
+                builder.Services.AddPersistance(builder.Configuration);
+                builder.Services.AddInfrastructure(builder.Configuration);
+                builder.Services.AddCors(options => options.AddPolicy(name: "MyAllowSpecificOrigins",
+                    builder =>
+                    {
+                        builder.WithOrigins("https://localhost:44376", "https://localhost:7267");
+                    }));
+
+                builder.Services.AddControllers();
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen(c => SwaggerDocExtensions.SetSwaggerDoc(c));
+                builder.Services.AddHealthChecks();
+
+                var app = builder.Build();
+                Log.Information("Application is starting up");
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHealthChecks("/hc");
+
+                app.UseHttpsRedirection();
+
+                app.UseSerilogRequestLogging();
+
+                app.UseCors();
+
+                app.UseAuthorization();
+
+                app.MapControllers();
+
+                app.Run();
+
             }
-
-            app.UseHealthChecks("/hc");
-
-            app.UseHttpsRedirection();
-
-            app.UseCors();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Could not start up application");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
